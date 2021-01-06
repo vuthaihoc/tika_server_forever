186436f3577e61238cd9d1a9cf6a40e81970d1a5
Stupid Dev <vuthaihoc.ttud@gmail.com>
2021-01-06 17:36:57 +0700

    Limit max time

--- check_tika_health.sh
+++ check_tika_health.sh
@@ -4,7 +4,7 @@ TIKA_URL="http://127.0.0.1:9998/tika"
 
 sleep 5
 
-RESPONSE=$(curl --write-out "%{http_code}\n" --silent --output /dev/null -T test.pdf ${TIKA_URL})
+RESPONSE=$(curl -m 5 --write-out "%{http_code}\n" --silent --output /dev/null -T test.pdf ${TIKA_URL})
 
 if [ $RESPONSE != '200' ]
 then
